23e5bc12717e9f4047287b53c67107d8170a5dd4
lz-junior <correajr80@hotmail.com>
2023-07-01 22:56:14 -0300

    add img

--- react-landing-page/src/App.js
+++ react-landing-page/src/App.js
@@ -1,11 +1,14 @@
 import './index.css';
-import RoutesApp from './routes'
+import RoutesApp from './routes';
+
+import Circulo from './images/circulo.png';
 
 function App() {
   return (
     <div>
       <RoutesApp/>
       <h2>Welcome</h2>
+      <img src={Circulo}/>
     </div>
   );
 }
