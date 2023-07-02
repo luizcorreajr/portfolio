9fd09f1a14ecb0063cf40a6522a081a26df111e4
lz-junior <correajr80@hotmail.com>
2023-07-02 19:29:28 -0300

    variaveis css

--- react-landing-page/src/pages/Home/index.js
+++ react-landing-page/src/pages/Home/index.js
@@ -1,9 +1,12 @@
+import ImgCirculo from './../../images/circulo.png';
+import './home.css';
 
 
 function Home() {
   return (
     <div>
       <h1>Página HOME</h1>
+      <img src={ImgCirculo} className="img-circulo" alt="circulo"/>
     </div>
   )
 }
