51bade7e2f01e2f343ab9822fab0ff56ae73457d
lz-junior <correajr80@hotmail.com>
2023-07-12 22:12:11 -0300

    page Sobre

--- react-landing-page/src/pages/Home/index.js
+++ react-landing-page/src/pages/Home/index.js
@@ -6,6 +6,7 @@ function Home() {
   return (
     <div>
       <h1>Página HOME</h1>
+      {/* dinamizar o circulo quando o mouse passar em cima */}
       {/* <img src={ImgCirculo} className="img-circulo" alt="circulo"/> */}
     </div>
   )
