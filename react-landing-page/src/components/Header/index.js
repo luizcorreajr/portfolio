507e7a49660b7f6a270e1840506a271ed89c5444
lz-junior <correajr80@hotmail.com>
2023-07-14 22:18:50 -0300

    dropdown - janela flutuante

--- react-landing-page/src/components/Header/index.js
+++ react-landing-page/src/components/Header/index.js
@@ -6,12 +6,25 @@ function Header() {
     <div>
       <header>
         <Link to="/" className="logo">I'mJr</Link>
-        <div className="link-group">
-          <Link to="/" className="link-header">Home</Link>
-          <Link to="https://github.com/lz-junior" className="link-header" target="_blank">GitHub</Link>
-          <Link to="https://www.linkedin.com/in/luizcorreajr/" className="link-header" target="_blank">LinkedIn</Link>
-          <Link to="/projects" className="link-header">Projects</Link>
-        </div>
+
+        <nav>
+          <ul className="link-group">
+            
+            <li><Link to="/" className="link-header">Home</Link></li>
+            <li><Link to="https://github.com/lz-junior" className="link-header" target="_blank">GitHub</Link></li>
+            <li><Link to="https://www.linkedin.com/in/luizcorreajr/" className="link-header" target="_blank">LinkedIn</Link></li>
+            <li className="dropdown">
+              <Link to="/projects" className="link-header">Projects</Link>
+              <div className="dropdown-menu">
+                <a href="/">teste 1</a>
+                <a href="/">teste 2</a>
+                <a href="/">teste 3</a>
+              </div>
+            </li>
+            
+          </ul>
+        </nav>
+
       </header>
     </div>
   )
