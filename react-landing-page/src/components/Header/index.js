1e0160bca8085a4607c109a8f811dc3ab528a0a4
lz-junior <correajr80@hotmail.com>
2023-07-21 18:11:24 -0300

    btn menu 0.5

--- react-landing-page/src/components/Header/index.js
+++ react-landing-page/src/components/Header/index.js
@@ -6,19 +6,25 @@ import './header.css';
 import { FaAlignJustify } from 'react-icons/fa';
 
 function Header() {
+
+  function btnMenu() {
+    let divOptions = document.getElementById("div-options");
+    divOptions.style.display = 'block';
+  }
+
   return (
     <div>
       <header>
 
         <div className="div-header">
           <Link to="/" className="logo">I'mJr</Link>
-          <button className="icon-menu"><FaAlignJustify/></button>
+          <button onClick={()=> btnMenu()} className="icon-menu"><FaAlignJustify/></button>
         </div>
 
-        <div className="div-options">
+
+        <div id="div-options" className="div-options">
           <LinkHeader/>
         </div>
-
       </header>
     </div>
   )
