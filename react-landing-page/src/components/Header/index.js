0326b9b343a008894e942833b1a0e16c3b20f5c8
lz-junior <correajr80@hotmail.com>
2023-07-20 22:36:35 -0300

    component LinkHeader

--- react-landing-page/src/components/Header/index.js
+++ react-landing-page/src/components/Header/index.js
@@ -1,3 +1,5 @@
+import LinkHeader from '../LinkHeader';
+
 import {Link} from 'react-router-dom';
 
 import './header.css';
@@ -7,29 +9,13 @@ function Header() {
   return (
     <div>
       <header>
-        <Link to="/" className="logo">I'mJr</Link>
 
-        <nav className="container">
+        <div className="div-header">
+          <Link to="/" className="logo">I'mJr</Link>
           <FaAlignJustify className="icon-menu"/>
+        </div>
 
-          <ul className="link-group">
-            
-            <li><Link to="/" className="link-header">Home</Link></li>
-            <li><Link to="https://github.com/lz-junior" className="link-header" target="_blank">GitHub</Link></li>
-            <li><Link to="https://www.linkedin.com/in/luizcorreajr/" className="link-header" target="_blank">LinkedIn</Link></li>
-            <li className="dropdown">
-              
-              <Link to="/projects" className="link-header">Projects</Link>
-              
-              <div className="dropdown-menu">
-                <a href="https://class-react-ljr.netlify.app" className="link-header" target="_blank" rel="noopener noreferrer">Prime Flix</a>
-                <a href="/" className="link-header">teste 002</a>
-                <a href="/" className="link-header">teste 003</a>
-              </div>
-              
-            </li>
-          </ul>
-        </nav>
+        <LinkHeader/>
 
       </header>
     </div>
--- react-landing-page/src/components/LinkHeader/index.js
+++ react-landing-page/src/components/LinkHeader/index.js
@@ -1,9 +1,27 @@
-
+import {Link} from 'react-router-dom';
+import './linkHeader.css';
 
 function LinkHeader() {
   return (
-    <div>
-      
+    <div className="div-container">
+      <nav className="container">
+        <ul className="link-group">
+            
+          <li><Link to="/" className="link-header">Home</Link></li>
+          <li><Link to="https://github.com/lz-junior" className="link-header" target="_blank">GitHub</Link></li>
+          <li><Link to="https://www.linkedin.com/in/luizcorreajr/" className="link-header" target="_blank">LinkedIn</Link></li>
+          <li className="dropdown">
+            
+            <Link to="/projects" className="link-header">Projects</Link>
+            
+            <div className="dropdown-menu">
+              <a href="https://class-react-ljr.netlify.app" className="link-header" target="_blank" rel="noopener noreferrer">Prime Flix</a>
+              <a href="/" className="link-header">teste 002</a>
+              <a href="/" className="link-header">teste 003</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
