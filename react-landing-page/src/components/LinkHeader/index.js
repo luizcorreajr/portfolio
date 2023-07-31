ab10a1a53108de6a82cd7e9e5c9e50c42606eb45
lz-junior <correajr80@hotmail.com>
2023-07-30 22:41:59 -0300

    commit PC

--- react-landing-page/src/components/LinkHeader/index.js
+++ react-landing-page/src/components/LinkHeader/index.js
@@ -12,7 +12,7 @@ function LinkHeader() {
           <li className="li-link-header"><Link to="https://github.com/lz-junior" className="link-header" target="_blank">GitHub</Link></li>
           <li className="li-link-header"><Link to="https://www.linkedin.com/in/luizcorreajr/" className="link-header" target="_blank">LinkedIn</Link></li>
           <li className="li-link-header dropdown">
-            
+
             <Link to="/projects" className="link-header">Projects</Link>
             
             <ul className="dropdown-menu">
@@ -21,7 +21,7 @@ function LinkHeader() {
               <li href="/" className="link-header">teste 003</li>
             </ul>
           </li>
-          
+
         </ul>
       </nav>
     </div>
