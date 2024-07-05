b86477f9e473aba6d348fe2ef6eef98cc940f232
lz-junior <correajr.dev@gmail.com>
2024-07-04 23:38:28 -0300

    header component

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import Header from '../components/Header';
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World!</h1>
+      <Header/>
     </main>
   )
 }
---
+++ portfolio/src/components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </header>
+  )
+}
