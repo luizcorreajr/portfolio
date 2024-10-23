2d30baeda71865748cda31d24049539830201db4
lz-junior <correajr.dev@gmail.com>
2024-10-22 23:44:39 -0300

    styled.ts sizes

---
+++ portfolio/src/styles/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+interface ISize {
+  mobile: string;
+  tablet: string;
+  desktopS: string;
+  desktopL: string;
+}
+const size: ISize = {
+  mobile: "360px",
+  tablet: "640px",
+  desktopS: "768px",
+  desktopL: "1086px",
+};
+
+export const Container = styled.div`
+
+`;
+
+export const Content = styled.div`
+
+`;
